b1b41d67d3358cc6e9bd6f8bbc1d76d4ded47952
Andrew Stakhov <astakhov@pivotal.io>
2019-04-24 15:31:12 -0400

    Cleanup, add build scripts

---
+++ src/MyBuildpack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyBuildpack
+{
+    public class MyBuildpack : SupplyBuildpack
+    {
+        static int Main(string[] args)
+        {
+            return new MyBuildpack().Run(args);
+        }
+        protected override bool Detect(string buildPath)
+        {
+            return false;
+        }
+
+        protected override void Apply(string buildPath, string cachePath, string depsPath, int index)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
